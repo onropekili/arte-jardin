1702288a7854848292a1298a0793e23169370d23
Cristopher Yahir Alvarado Mombela <cristopher.alvarado.21s@utzmg.edu.mx>
2025-08-14 18:42:06 -0600

    added modules for it to be static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+    output: 'server', // modo servidor
+  adapter: node({ mode: 'standalone' }), 
   integrations: [tailwind()]
 });
