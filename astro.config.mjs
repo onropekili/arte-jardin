3808fe227058eea4d25362da69db1a6b82befbc2
Cristopher Yahir Alvarado Mombela <cristopher.alvarado.21s@utzmg.edu.mx>
2025-08-14 18:58:02 -0600

    hotfix not a node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-    output: 'server', // modo servidor
-  adapter: node({ mode: 'standalone' }), 
   integrations: [tailwind()]
 });
